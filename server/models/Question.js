b2d7708a855c1444af84fb7d591814a20069b05a
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-10 12:14:13 -0500

    created Question model

---
+++ server/models/Question.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const questionSchema = new Schema({
+    category: {
+        type: String
+    },
+    question: {
+        type: String,
+        required: true
+    },
+    correct: {
+        type: String,
+        required: true
+    },
+    incorrect: [
+        {
+            type: String
+        }
+    ],
+    quiz: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Quiz'
+        }
+    ],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+});
+
+const Question = model('Question', questionSchema);
+
+module.exports = Question;
