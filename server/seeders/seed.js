9046a715ee18240a406b839d5bdcae8d53efca1b
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-10 11:50:29 -0500

    created questionSeeds.js

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,7 @@
 const db = require('../config/connection');
 const { User, Thought } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const questionSeeds = require('./questionSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
@@ -11,8 +11,8 @@ db.once('open', async () => {
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < questionSeeds.length; i++) {
+      const { _id, thoughtAuthor } = await Thought.create(questionSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: thoughtAuthor },
         {
