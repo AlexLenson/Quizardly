d7dae3705670fc359e6e01360206c83598097766
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-12-10 13:21:15 -0500

    updated model index

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
-const Thought = require('./Thought');
+const Quiz = require('./Quiz');
+const Question = require('./Question');
 
-module.exports = { User, Thought };
+module.exports = { User, Quiz, Question };
