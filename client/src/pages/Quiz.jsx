195a289500584c7b30e66eec225eeaafcbc6a0f5
cpicha20 <cpichardowps@gmail.com>
2023-12-12 22:33:44 -0500

    logic for running the quiz, probably doesnt work

---
+++ client/src/pages/Quiz.jsx
@@ -0,0 +1,89 @@
+// Import the `useParams()` hook
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_SINGLE_QUIZ } from '../utils/queries';
+
+const Quiz = () => {
+  // Use `useParams()` to retrieve value of the route parameter `:quizId`
+  const { quizId } = useParams();
+
+  const { loading, data } = useQuery(QUERY_SINGLE_QUIZ, {
+    // pass URL parameter
+    variables: { quizId: quizId },
+  });
+
+  const quiz = data?.quiz || {};
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const multipleChoice = (index)=>{
+    //take the correct answers and push it into the incorrect answers array to create the choices array
+  const correct_answer=  quiz.questions[index].correct_answer;
+  const choices = quiz.questions[index].incorrect_answers.push(correct_answer);
+
+//shuffle array function
+  const shuffleArray = (array) => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+    }
+    return shuffledArray;
+  };
+
+  //shuffle choices
+  const shuffledAnswers = shuffleArray(choices);
+  return shuffledAnswers;
+  };
+  
+  //click 
+  const handleAnswerClick = (selectedAnswer) => {
+    const updatedAnswers = [...userAnswers];
+    updatedAnswers[currentQuestion] = selectedAnswer;
+    setUserAnswers(updatedAnswers);
+    NextQuestion();
+  };
+
+  const NextQuestion = () => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="my-3">
+      <h3 className="card-header bg-dark text-light p-2 m-0">
+        {quiz.title} 
+      </h3>
+      <div className="bg-light py-4">
+        <blockquote
+          className="p-4"
+          style={{
+            fontSize: '1.5rem',
+            fontStyle: 'italic',
+            border: '2px dotted #1a1a1a',
+            lineHeight: '1.5',
+          }}
+        >
+          {quiz.questions.question}
+        </blockquote>
+      </div>
+      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+      <ul>
+        {multipleChoice(currentQuestion).map((answer, index) => (
+          <li key={index}>{answer}</li>
+        ))}
+      </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
