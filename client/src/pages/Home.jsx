76568b4789e34ee4483e9650fed161e3f3f9c261
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-13 11:53:02 -0500

    linked all images to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import { useQuery } from '@apollo/client';
+import Quizardly from '../assets/quizardly.png'
 
+import General from '../assets/GeneralImage.jpg'
+import History from '../assets/HistoryImage.jpg'
+import Games from '../assets/GamesImage.jpg'
+import TVMovies from '../assets/TVImage.jpg'
+import Literature from '../assets/LitImage.jpg'
+import Tech from '../assets/TechImg.png'
+import Science from '../assets/ScienceImage.jpg'
+import Math from '../assets/MathImage.webp'
+import PopCulture from '../assets/PopCultureImage.jpg'
+import Music from '../assets/MusicImage.jpg'
+import Food from '../assets/FoodImage.jpg'
+import Geography from '../assets/GeographyImage.jpg'
+import Sports from '../assets/TVImage.jpg'
 const Home = () => {
 
 
@@ -8,8 +22,10 @@ const Home = () => {
       <div className="flex-row justify-center">
         <div
           className="col-12 col-md-10 mb-3 p-3"
-          style={{ border: '1px dotted #1a1a1a' }}
+          // style={{ border: '1px dotted #1a1a1a' }}
         >
+         
+
 
         </div>
         <div className="col-12 col-md-8 mb-3">
