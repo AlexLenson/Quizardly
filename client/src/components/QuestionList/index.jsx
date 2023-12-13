7abbb52647b985985389985cb1e985ed67a38135
cpicha20 <cpichardowps@gmail.com>
2023-12-12 20:05:53 -0500

    refactored CommentList into QuestionList

--- client/src/components/QuestionList/index.jsx
+++ client/src/components/QuestionList/index.jsx
@@ -1,5 +1,5 @@
-const CommentList = ({ comments = [] }) => {
-  if (!comments.length) {
+const CommentList = ({questions = [] }) => {
+  if (!questions.length) {
     return <h3>No Comments Yet</h3>;
   }
 
@@ -12,17 +12,14 @@ const CommentList = ({ comments = [] }) => {
         Comments
       </h3>
       <div className="flex-row my-4">
-        {comments &&
-          comments.map((comment) => (
-            <div key={comment._id} className="col-12 mb-3 pb-3">
+        {questions &&
+          questions.map((question) => (
+            <div key={question._id} className="col-12 mb-3 pb-3">
               <div className="p-3 bg-dark text-light">
                 <h5 className="card-header">
-                  {comment.commentAuthor} commented{' '}
-                  <span style={{ fontSize: '0.825rem' }}>
-                    on {comment.createdAt}
-                  </span>
+                  {question.question}
                 </h5>
-                <p className="card-body">{comment.commentText}</p>
+                <p className="card-body">{question.correct_answero}</p>
               </div>
             </div>
           ))}
